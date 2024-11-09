b8a8350df3fcd320a76fbb95380d8ddd06742bd1
Biswajit Sethy <114790482+Biswajit6844@users.noreply.github.com>
2024-11-10 04:04:59 +0530

    Update views.py

--- app/views.py
+++ app/views.py
@@ -18,36 +18,55 @@ model = models.load_model('app/static/model/bird_species.h5')
 @app.route("/", methods=["GET", "POST"])
 def index():
 
-	# Execute if request is get
-	if request.method == "GET":
-		full_filename =  'images/white_bg.jpg'
-		return render_template("index.html", full_filename = full_filename)
+    # Execute if request is GET
+    if request.method == "GET":
+        full_filename = 'images/white_bg.jpg'
+        return render_template("index.html", full_filename=full_filename)
 
-	# Execute if reuqest is post
-	if request.method == "POST":
+    # Execute if request is POST
+    if request.method == "POST":
+        try:
+            # Check if file is uploaded
+            if 'image_upload' not in request.files:
+                error_message = "No file part in the request"
+                return render_template("index.html", error=error_message)
 
-		# Generating unique image name
-		letters = string.ascii_lowercase
-		name = ''.join(random.choice(letters) for i in range(10)) + '.png'
-		full_filename =  'uploads/' + name
+            image_upload = request.files['image_upload']
 
-		# Reading, resizing, saving and preprocessing image for predicition 
-		image_upload = request.files['image_upload']
-		imagename = image_upload.filename
-		image = Image.open(image_upload)
-		image = image.resize((224,224))
-		image.save(os.path.join(app.config['INITIAL_FILE_UPLOADS'], name))
-		image_arr = np.array(image.convert('RGB'))
-		image_arr.shape = (1,224,224,3)
+            # Check if a file is selected
+            if image_upload.filename == '':
+                error_message = "No selected file"
+                return render_template("index.html", error=error_message)
 
-		# Predicting output
-		result = model.predict(image_arr)
-		ind = np.argmax(result)
-		classes = ['AMERICAN GOLDFINCH', 'BARN OWL', 'CARMINE BEE-EATER', 'DOWNY WOODPECKER', 'EMPEROR PENGUIN', 'FLAMINGO']
+            # Generating unique image name
+            letters = string.ascii_lowercase
+            name = ''.join(random.choice(letters) for i in range(10)) + '.png'
+            full_filename = 'uploads/' + name
 
-		# Returning template, filename, extracted text
-		return render_template('index.html', full_filename = full_filename, pred = classes[ind])
+            # Reading, resizing, saving, and preprocessing image for prediction
+            image = Image.open(image_upload)
+            image = image.resize((224, 224))
+            image.save(os.path.join(app.config['INITIAL_FILE_UPLOADS'], name))
+            image_arr = np.array(image.convert('RGB'))
+            image_arr.shape = (1, 224, 224, 3)
+
+            # Predicting output
+            result = model.predict(image_arr)
+            ind = np.argmax(result)
+            classes = [
+                'AMERICAN GOLDFINCH', 'BARN OWL', 'CARMINE BEE-EATER',
+                'DOWNY WOODPECKER', 'EMPEROR PENGUIN', 'FLAMINGO'
+            ]
+
+            # Returning template with prediction result
+            return render_template('index.html', full_filename=full_filename, pred=classes[ind])
+
+        except Exception as e:
+            # Log the error and return a default response
+            error_message = f"An error occurred: {str(e)}"
+            return render_template("index.html", error=error_message)
 
 # Main function
 if __name__ == '__main__':
     app.run(debug=True)
+
